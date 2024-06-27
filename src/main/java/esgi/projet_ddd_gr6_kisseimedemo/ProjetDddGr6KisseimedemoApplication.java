89f93736f04a3374a310d4f577323dca37fbfdd8
trofinn <trofin.nicu98@gmail.com>
2024-06-27 12:32:18 +0200

    initial

---
+++ src/main/java/esgi/projet_ddd_gr6_kisseimedemo/ProjetDddGr6KisseimedemoApplication.java
@@ -0,0 +1,13 @@
+package esgi.projet_ddd_gr6_kisseimedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDddGr6KisseimedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDddGr6KisseimedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esgi/projet_ddd_gr6_kisseimedemo/ProjetDddGr6KisseimedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package esgi.projet_ddd_gr6_kisseimedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDddGr6KisseimedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
